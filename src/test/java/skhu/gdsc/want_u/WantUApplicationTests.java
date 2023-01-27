1938f4c004b887b6ee3598cee49c8601bfcfa947
dd-jiyun <dlwldbs8015@gmail.com>
2023-01-28 00:20:03 +0900

    modified.test annotation

--- src/test/java/skhu/gdsc/want_u/WantUApplicationTests.java
+++ src/test/java/skhu/gdsc/want_u/WantUApplicationTests.java
@@ -3,7 +3,6 @@ package skhu.gdsc.want_u;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class WantUApplicationTests {
 
 	@Test
